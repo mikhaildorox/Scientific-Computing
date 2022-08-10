b472deffd6d93c7f380ef521686d944e1b735159
Mikhail Doroshenko <mixdorosh@gmail.com>
2022-08-11 03:12:58 +0700

    def arithmetic_arranger

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
-#Scientific computing with python
+# Scientific computing with python
 
-test = '25 + 100' 
+expressions = ['25 + 10000', '150 - 10']
 answer = True
+chars = '-+'
 
-def arithmetic_arranger(test, answer):
-  test = 1
+
+def arithmetic_arranger(expressions):
+    result = 0
+    for expression in expressions:
+        if chars[0] or chars[1] in expression:
+            expr_split = (expression.split())
+            if '+' in expr_split:
+                result = int(expr_split[0]) + int(expr_split[2])
+            elif '-' in expr_split:
+                result = int(expr_split[0]) - int(expr_split[2])
+            print(f'{expr_split[0]}\n{expr_split[1]} {expr_split[2]}')
+            print('result', result)
+        else:
+            print('no -+')
+
+
+arithmetic_arranger(expressions)
